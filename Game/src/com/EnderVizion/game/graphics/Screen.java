ec05476c843d01c5a81a4d5ded6e34051b193eea
LukeJansen <lpjansen@live.co.uk>
2014-06-26 14:46:04 +0100

    Prepared for needing to clear screen!

--- Game/src/com/EnderVizion/game/graphics/Screen.java
+++ Game/src/com/EnderVizion/game/graphics/Screen.java
@@ -10,11 +10,17 @@ public class Screen {
 		this.height = height;
 		pixels = new int[width * height];
 	}
+	
+	public void clear(){
+		for (int i = 0; i < pixels.length; i++){
+			pixels[i] = 0;
+		}
+	}
 
 	public void render() {
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
-				pixels[100 + 1000 * width] = 0xff00ff;
+				pixels[x + y * width] = 0xff00ff;
 			}
 		}
 	}
