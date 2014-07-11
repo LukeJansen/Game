f76116d8a0a980dd915395e7576019b3b79a601d
LukeJansen <lpjansen@live.co.uk>
2014-07-11 21:35:06 +0100

    Moving Tiles

--- Game/src/com/EnderVizion/game/graphics/Screen.java
+++ Game/src/com/EnderVizion/game/graphics/Screen.java
@@ -6,8 +6,10 @@ public class Screen {
 
 	private int width, height;
 	public int[] pixels;
-
-	public int[] tiles = new int[64 * 64];
+	public final int MAP_SIZE = 32;
+	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
+	
+	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
 
 	private Random random = new Random();
 
@@ -16,7 +18,7 @@ public class Screen {
 		this.height = height;
 		pixels = new int[width * height];
 
-		for (int i = 0; i < 64 * 64; i++) {
+		for (int i = 0; i < MAP_SIZE * MAP_SIZE; i++) {
 			tiles[i] = random.nextInt(0xffffff);
 		}
 	}
@@ -27,13 +29,14 @@ public class Screen {
 		}
 	}
 
-	public void render() {
+	public void render(int xOffset, int yOffset) {
 		for (int y = 0; y < height; y++) {
+			int yy = y + yOffset;
+			//if (y < 0 || y >= width) break;
 			for (int x = 0; x < width; x++) {
-				if (x < 0 || x >= width){
-					break;
-				}
-				int tileIndex = (x >> 4) + (y >> 4) * 64;
+				int xx = x + xOffset;
+				//if (x < 0 || x >= width) break;
+				int tileIndex = ((xx >> 4) & MAP_SIZE_MASK) + ((yy >> 4)&MAP_SIZE_MASK) * MAP_SIZE;
 				pixels[x + y * width] = tiles[tileIndex];
 			}
 		}
