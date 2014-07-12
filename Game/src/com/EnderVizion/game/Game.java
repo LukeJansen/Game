f70d0776f03ab71269b0c75e30f2b9569226dec2
LukeJansen <lpjansen@live.co.uk>
2014-07-12 15:09:19 +0100

    New Music from matt! Getting ready to add in levels!

--- Game/src/com/EnderVizion/game/Game.java
+++ Game/src/com/EnderVizion/game/Game.java
@@ -118,7 +118,7 @@ public class Game extends Canvas implements Runnable {
 		g.dispose();
 		bs.show();
 	}
-
+	
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
