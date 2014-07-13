74101068fac5ec32c1cbb325627c034214f30b39
LukeJansen <lpjansen@live.co.uk>
2014-07-13 18:03:27 +0100

    Added renderTile method!

--- Game/src/com/EnderVizion/game/graphics/Screen.java
+++ Game/src/com/EnderVizion/game/graphics/Screen.java
@@ -2,6 +2,8 @@ package com.EnderVizion.game.graphics;
 
 import java.util.Random;
 
+import com.EnderVizion.game.level.tile.Tile;
+
 public class Screen {
 
 	private int width, height;
@@ -37,6 +39,17 @@ public class Screen {
 				int xp = x + xOffset;
 				if (xp < 0 || xp>= width) continue;
 				pixels[xp + yp * width] = Sprite.grass.pixels[(x&15) + (y&15) * Sprite.grass.SIZE];
+			}
+		}
+	}
+	
+	public void renderTile(int xp, int yp, Tile tile){
+		for (int y = 0; y < tile.sprite.SIZE; y++){
+			int ya = y + yp;
+			for (int x = 0; x < tile.sprite.SIZE; x++){
+				int xa = x + xp;
+				if (xa < 0 || xa >= width || ya < 0 || ya >= width) break;
+				pixels[xa+ya*width] = tile.sprite.pixels[x+y*tile.sprite.SIZE];
 			}
 		}
 	}
