0b4a9f0df444e615754ea85cf9205c61977aaf8f
LukeJansen <lpjansen@live.co.uk>
2014-07-13 14:49:01 +0100

    Added grass tile!

---
+++ Game/src/com/EnderVizion/game/level/tile/GrassTile.java
@@ -0,0 +1,15 @@
+package com.EnderVizion.game.level.tile;
+
+import com.EnderVizion.game.graphics.Screen;
+import com.EnderVizion.game.graphics.Sprite;
+
+public class GrassTile extends Tile{
+
+	public GrassTile(Sprite sprite) {
+		super(sprite);
+	}
+	
+	public void Render(int x, int y, Screen screen){
+		// Do render stuff here!
+	}
+}
