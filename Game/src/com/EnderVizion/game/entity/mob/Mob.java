463098506206c0cb88788f6aaf6727975e6cf9b7
LukeJansen <lpjansen@live.co.uk>
2014-08-02 16:13:09 +0100

    Added Mob Framwork!

---
+++ Game/src/com/EnderVizion/game/entity/mob/Mob.java
@@ -0,0 +1,21 @@
+package com.EnderVizion.game.entity.mob;
+
+import com.EnderVizion.game.entity.Entity;
+import com.EnderVizion.game.graphics.Sprite;
+
+public abstract class Mob extends Entity{
+	
+	protected Sprite sprite;
+	protected int dir = 0;
+	protected boolean moving  = false;
+	
+	public void move(){
+	}
+	
+	public void update(){
+	}
+	
+	private boolean collision(){
+		return false;
+	}
+}
